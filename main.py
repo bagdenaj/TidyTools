2b819e8511467cad7310473cf58763b3ab26a4ff
bagdenaj <jo.bagd@posteo.de>
2022-06-11 17:19:57 +0200

    Add logging

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from kivymd.uix.list import OneLineListItem
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from jnius import autoclass
+from kivy.logger import Logger
 
 from kivy.utils import platform
 
-if platform == 'android':
-    from jnius import autoclass, cast
-    JS = autoclass('java.lang.String')
-    Intent = autoclass('android.content.Intent')
-    PythonActivity = autoclass('org.kivy.android.PythonActivity')
-    currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
+# if platform == 'android':
+#     from jnius import autoclass, cast
+#     JS = autoclass('java.lang.String')
+#     Intent = autoclass('android.content.Intent')
+#     PythonActivity = autoclass('org.kivy.android.PythonActivity')
+#     currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
 
 KV = '''
 <ManufacturesSelect>:
@@ -91,71 +92,93 @@ tools = [
 ]
 
 
-# def set_schedule(title):
-#     System = autoclass ('java.lang.System')
-#     intent = autoclass('android.content.Intent')
-#     Calendar = autoclass('java.util.Calendar')
-#     calendar = Calendar.getInstance()
-#     calendar.setTimeInMillis(System.currentTimeMillis())
-#     intent.setType("vnd.android.cursor.item/event")
-#     intent.putExtra(Events.TITLE, title)
-#     intent.putExtra(Events.RRULE, "FREQ=WEEKLY;BYDAY=MO;COUNT=3")
-#     intent.putExtra("title", "A Test Event from android app")
-#     intent.setAction(Intent.ACTION_INSERT)
-#     PythonActivity.mActivity.startActivity(intent)
+def set_schedule(title):
+    try:
+        System = autoclass ('java.lang.System')
+        intent = autoclass('android.content.Intent')
+        Calendar = autoclass('java.util.Calendar')
+        calendar = Calendar.getInstance()
+        calendar.setTimeInMillis(System.currentTimeMillis())
+        intent.setType("vnd.android.cursor.item/event")
+        intent.putExtra(Events.TITLE, title)
+        intent.putExtra(Events.RRULE, "FREQ=WEEKLY;BYDAY=MO;COUNT=3")
+        intent.putExtra("title", "A Test Event from android app")
+        intent.setAction(Intent.ACTION_INSERT)
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        PythonActivity.mActivity.startActivity(intent)
+    except Exception as err:
+            Logger.exception(err)
 
 class ManufacturesSelect(Screen):
     def __init__(self, **kwargs) -> None:
         super(ManufacturesSelect, self).__init__(**kwargs)
 
     def add_toolselect(self):
-        toolselect = ToolSelect()
-        self.ids.tool_select.add_widget(toolselect)
+        try:
+            toolselect = ToolSelect()
+            self.ids.tool_select.add_widget(toolselect)
+        except Exception as err:
+            Logger.exception(err)
 
     def pressed(self, value):
-        self.ids.manufacture.clear_widgets()
-        self.ids.search_field.text = value.text + " "
-        self.add_toolselect()
+        try:
+            self.ids.manufacture.clear_widgets()
+            self.ids.search_field.text = value.text + " "
+            self.add_toolselect()
+        except Exception as err:
+            Logger.exception(err)
 
 
     def set_manf_list(self, text=" "):
-        self.ids.tool_select.clear_widgets()
-        self.ids.manufacture.clear_widgets()
-        for man in manufactures:
-            if text.casefold() in man.casefold():
-                self.ids.manufacture.add_widget(
-                    OneLineListItem(text=man,on_press=self.pressed)
-                )
+        try:
+            self.ids.tool_select.clear_widgets()
+            self.ids.manufacture.clear_widgets()
+            for man in manufactures:
+                if text.casefold() in man.casefold():
+                    self.ids.manufacture.add_widget(
+                        OneLineListItem(text=man,on_press=self.pressed)
+                    )
+        except Exception as err:
+            Logger.exception(err)
 
 class ToolSelect(BoxLayout):
     def __init__(self, **kwargs) -> None:
         super(ToolSelect, self).__init__(**kwargs)
 
     def pressed(self, value):
-        self.ids.tools.clear_widgets()
-        self.ids.search_tool.text = value.text + " "
-        # need to somehow disable set_list() after here
-        # set_schedule(value.text)
+        try:
+            self.ids.tools.clear_widgets()
+            self.ids.search_tool.text = value.text + " "
+            # need to somehow disable set_list() after here
+            set_schedule(value.text)
+        except Exception as err:
+            Logger.exception(err)
 
 
 
     def set_tools_list(self, text=" "):
-        self.ids.tools.clear_widgets()
-        for tool in tools:
-            if text.casefold() in tool.casefold():
-                self.ids.tools.add_widget(
-                    OneLineListItem(text=tool,on_press=self.pressed)
-                )
+        try:
+            self.ids.tools.clear_widgets()
+            for tool in tools:
+                if text.casefold() in tool.casefold():
+                    self.ids.tools.add_widget(
+                        OneLineListItem(text=tool,on_press=self.pressed)
+                    )
+        except Exception as err:
+            Logger.exception(err)
 
 
 class Test(MDApp):
     def build(self):
+        Logger.info("Building app")
         Builder.load_string(KV)
         
         self.screen = ManufacturesSelect(name="manufacture_select")
+        Logger.info("Screen instanciated")
         return self.screen
 
     def on_start(self):
+        Logger.info("On_start")
         self.screen.set_manf_list()
 
 Test().run()
