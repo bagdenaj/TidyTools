1c011e80fa0d0fd155c8a5f672f461c14fec2e7c
bagdenaj <jo.bagd@posteo.de>
2022-06-16 20:59:08 +0200

    Convert python int to java long

--- main.py
+++ main.py
@@ -193,6 +193,7 @@ class ToolSelect(BoxLayout):
             CalendarContract = autoclass("android.provider.CalendarContract")
             Events = autoclass("android.provider.CalendarContract$Events")
             JS = autoclass("java.lang.String")
+            Long = autoclass("java.lang.Long")
             self.intent = Intent()
 
             self.begin_time = Calendar.getInstance()
@@ -206,11 +207,11 @@ class ToolSelect(BoxLayout):
             Logger.info(f"end_time: {self.end_time.getTimeInMillis()}")
             self.intent.putExtra(
                 CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                int(self.begin_time.getTimeInMillis()),
+                Long(self.begin_time.getTimeInMillis()),
             )
             self.intent.putExtra(
                 CalendarContract.EXTRA_EVENT_END_TIME,
-                int(self.end_time.getTimeInMillis()),
+                Long(self.end_time.getTimeInMillis()),
             )
             self.intent.putExtra(Events.DESCRIPTION, JS(self.description))
             self.intent.putExtra(Events.RRULE, JS(self.rrule))
