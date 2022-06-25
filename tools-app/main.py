7a3b40bd63ab593d291ba8f8431c80b36eea9aae
bagdenaj <jo.bagd@posteo.de>
2022-06-25 15:02:10 +0200

    Try casting to string;j

--- tools-app/main.py
+++ tools-app/main.py
@@ -112,11 +112,11 @@ def set_schedule(tool):
 
         intent.putExtra(
             CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-            cast("java.lang.Long", date.getTimeInMillis()),
+            cast("java/lang/String;L", date.getTimeInMillis()),
         )
         intent.putExtra(
             CalendarContract.EXTRA_EVENT_END_TIME,
-            cast("java.lang.Long", date.getTimeInMillis()),
+            cast("java/lang/String;J", date.getTimeInMillis()),
         )
 
         intent.putExtra(Events.DESCRIPTION, JS("Some description"))
