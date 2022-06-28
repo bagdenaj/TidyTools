0e7cae3b1d51f2625e3df07c6d7df533f8a76f62
bagdenaj <jo.bagd@posteo.de>
2022-06-28 14:46:04 +0200

    Try JL(float()).longValue()

--- tools-app/main.py
+++ tools-app/main.py
@@ -114,11 +114,11 @@ def set_schedule(tool):
 
         intent.putExtra(
             CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-            JL(float(date.getTimeInMillis())),
+            JL(float(date.getTimeInMillis())).longValue(),
         )
         intent.putExtra(
             CalendarContract.EXTRA_EVENT_END_TIME,
-            JL(float(date.getTimeInMillis())),
+            JL(float(date.getTimeInMillis())).longValue(),
         )
 
         intent.putExtra(Events.DESCRIPTION, JS("Some description"))
