9bdcb529255966ad108c8a1e4e360b0776d32902
bagdenaj <jo.bagd@posteo.de>
2022-07-14 10:46:11 +0200

    Add file to db support

--- tools-api/manf_query.py
+++ tools-api/manf_query.py
@@ -1,6 +1,9 @@
 import os
 
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 conn = psycopg2.connect(
     host="ec2-63-34-180-86.eu-west-1.compute.amazonaws.com",
@@ -11,13 +14,18 @@ conn = psycopg2.connect(
 
 manufactures = ["Stihl", "Obi", "ABUS", "Bosch", "HYMER"]
 
+
 cur = conn.cursor()
 cur.execute(
     "CREATE TABLE IF NOT EXISTS manufacture (id SERIAL PRIMARY KEY, name VARCHAR(255))"
 )
 
-for manuf in manufactures:
-    cur.execute("""INSERT INTO manufacture (name) VALUES (%s)""", (manuf,))
+with open("werkzeughersteller.txt", "r", encoding="utf-8") as file:
+    manufactures = file.readlines()
+    for manufactur in manufactures:
+        cur.execute(
+            """INSERT INTO manufacture (name) VALUES (%s)""", (manufactur.strip(),)
+        )
 
 conn.commit()
 cur.close()
--- tools-api/tool_query.py
+++ tools-api/tool_query.py
@@ -1,6 +1,11 @@
+import csv
 import os
 
 import psycopg2
+from dotenv import load_dotenv
+from tqdm import tqdm
+
+load_dotenv()
 
 conn = psycopg2.connect(
     host="ec2-63-34-180-86.eu-west-1.compute.amazonaws.com",
@@ -9,21 +14,22 @@ conn = psycopg2.connect(
     password=os.getenv("POSTGRES_PASSWORD"),
 )
 
-manufactures = ["Stihl", "Obi", "ABUS", "Bosch", "HYMER"]
-
-tools = ["Kettensäge", "Presslufthammer", "Schleifgerät", "Nagel"]
-
 cur = conn.cursor()
+cur.execute("""DROP TABLE IF EXISTS tools""")
+
 cur.execute(
     """CREATE TABLE IF NOT EXISTS tools (id SERIAL PRIMARY KEY, name VARCHAR(255), """
-    """manufacture VARCHAR(255), schedule VARCHAR(255))"""
+    """manufacture VARCHAR(255), schedule VARCHAR(255));"""
 )
-for tool in tools:
-    for manf in manufactures:
+
+
+with open("tools.csv", "r") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=";")
+    for row in tqdm(csv_reader):
         cur.execute(
             """INSERT INTO tools (name, manufacture, schedule) VALUES (%s, %s, %s)""",
-            (tool, manf, "Monthly"),
+            (row["Werkzeug"], row["Hersteller"], "Monthly"),
         )
 conn.commit()  # Commit the changes to the database
 cur.close()
-conn.close()
+conn
